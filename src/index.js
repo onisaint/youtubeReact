29d3d0e434a8a6b794ae1171dfac4386bb433333
Madhu Kumar D S <madhudskumar@live.in>
2017-07-16 15:32:09 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 //create new component
-
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import SearchBar from './components/search';
-
-
+import _yt from 'youtube-api-search';
 const API_KEY = 'AIzaSyCccYHWP9l2C3ufeWBZ_NM9_UkwhX9_Mp0';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-};
 
+
+class App extends Component {
+    constructor(props){
+        super();
+        this.state = {
+            videos: []
+        };
+        _yt({key: API_KEY, term: 'surfboards'}, (response) => {
+           this.setState({videos:response});
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App/>, document.getElementById('app'));
